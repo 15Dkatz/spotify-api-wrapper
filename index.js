703cf9e46a10b74abd7c9c750abb4f2e8e422136
David Katz <15Dkatz@shcp.edu>
2019-03-09 08:46:57 -0800

    Use public rate limiting middleware

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const request = require('request');
 const base64 = require('base-64');
-const LimitingMiddleware = require('./limiting-middleware');
+const LimitingMiddleware = require('limiting-middleware');
 
 const app = express();
-const limitingMiddleware = new LimitingMiddleware();
 
-app.use(limitingMiddleware.limitByIp());
+app.use(new LimitingMiddleware().limitByIp());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
